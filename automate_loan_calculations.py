dce6830cdb5cf1d2ce6e2366e53e05e84a3706b6
anuraggargLTI <64598470+anuraggargLTI@users.noreply.github.com>
2022-12-01 14:49:01 -0500

    Part 2: Automate the Calculations

---
+++ automate_loan_calculations.py
@@ -0,0 +1,6 @@
+loan_costs = [500, 600, 200, 1000, 450]
+number_of_loans = len(loan_costs)
+total_loan_amount = sum(loan_costs)
+average_loan_amount = total_loan_amount/number_of_loans
+
+print(f"Number of loans are :",number_of_loans)
